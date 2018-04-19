ae193733c98d5d9a362990176f2805507f7b2cc8
shinsumicco <shinsumicco@gmail.com>
2018-04-19 09:24:12 +0900

    Implement the parser for ssh configuration

---
+++ config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import yaml
+import logging
+from collections import namedtuple
+
+logger = logging.getLogger(__name__)
+
+ssh_cfg = namedtuple("ssh_cfg", ("host", "hostname", "user", "identity_file", "port"))
+
+
+class ConfigParser:
+    def __init__(self, fp_config):
+        # config file path
+        self.fp_config = os.path.realpath(os.path.expanduser(fp_config))
+
+        # check if the config file can be opened correctly
+        try:
+            with open(self.fp_config, "r") as fin:
+                # check if the config file can be parsed as YAML
+                try:
+                    self.yaml = yaml.load(fin)
+                except yaml.YAMLError as e:
+                    logger.fatal("Couldn't parse the config file '{}' as YAML.".format(self.fp_config))
+                    sys.exit(1)
+                logger.info("The config file '{}' has been imported.".format(self.fp_config))
+        except OSError as e:
+            logger.fatal("Couldn't find the config file '{}'.".format(e.filename))
+            sys.exit(1)
+
+        # ssh config dict
+        self.ssh_cfgs = {}
+        for cfg in self.yaml["ssh_cfgs"]:
+            # check if the keys of the dict are exist
+            try:
+                self.ssh_cfgs[cfg["Host"]] = \
+                    ssh_cfg(cfg["Host"], cfg["HostName"], cfg["User"], cfg["IdentityFile"], cfg["Port"])
+            except KeyError as e:
+                logger.fatal("Invalid config style: couldn't find the field '{}'. Will be terminated.".format(e))
+                sys.exit(1)
