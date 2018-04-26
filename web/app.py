cd8ed64317bbf9859860e52e4730d0b7eb432288
shinsumicco <shinsumicco@gmail.com>
2018-04-26 17:51:09 +0900

    Improve the coding style according to PEP8

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
-#-*-coding:utf-8-*-
-
-import json
+# -*-coding:utf-8-*-
 import os
+import sys
+import json
 import time
 import random
 
@@ -14,19 +14,29 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+    def data_received(self, chunk):
+        pass
+
+
 class GPUStatusHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         # self.send_initial_data()
         tornado.ioloop.IOLoop.current().add_timeout(time.time() + 1, self.send_gpu_status)
 
+    def on_message(self, message):
+        pass
+
+    def data_received(self, chunk):
+        pass
+
     def send_initial_data(self):
         self.write_message(
             json.dumps(
                 {
-                "Minerva":{
-                            "1":{"u":[*range(60)], "m": [*range(60)]},
-                            "2":{"u":[*range(60)], "m": [*range(60)]}
-                        }
+                    "Minerva": {
+                        "1": {"u": [*range(60)], "m": [*range(60)]},
+                        "2": {"u": [*range(60)], "m": [*range(60)]}
+                    }
                 }
             )
         )
@@ -35,21 +45,21 @@ class GPUStatusHandler(tornado.websocket.WebSocketHandler):
         self.write_message(
             json.dumps(
                 {
-                "Minerva":{
-                            "1":{"u":15 + random.randint(1,20), "m": 76+ random.randint(1,20)},
-                            "2":{"u":48+ random.randint(1,20), "m": 23+ random.randint(1,20)}
-                        }
+                    "Minerva": {
+                        "1": {"u": 15 + random.randint(1, 20), "m": 76 + random.randint(1, 20)},
+                        "2": {"u": 48 + random.randint(1, 20), "m": 23 + random.randint(1, 20)}
+                    }
                 }
             )
         )
         tornado.ioloop.IOLoop.current().add_timeout(time.time() + 1, self.send_gpu_status)
 
+
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/gpu_status", GPUStatusHandler)
-    ],
-    template_path=os.path.join(os.getcwd(),  "templates"),
-    static_path=os.path.join(os.getcwd(),  "static"),
+    (r"/gpu_status", GPUStatusHandler)],
+    template_path=os.path.join(os.getcwd(), "templates"),
+    static_path=os.path.join(os.getcwd(), "static"),
 )
 
 if __name__ == "__main__":
