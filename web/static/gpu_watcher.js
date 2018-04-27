186e6db8b208517cd7b2a393b7d7bf9472ed424d
Kenta Urano <vrano@ucl.nuee.nagoya-u.ac.jp>
2018-04-27 12:45:59 +0900

    initialization of 2x2 graph

--- web/static/gpu_watcher.js
+++ web/static/gpu_watcher.js
@@ -1,7 +1,10 @@
 // Wrapping in nv.addGraph allows for '0 timeout render', stores rendered charts in nv.graphs, and may do more in the future... it's NOT required
-var chart;
+var chart = {};
 var data;
 var chart_data = {};
+
+// last 2 are dummy (for now)
+var color_set = ["#00ff00", "#ff007f", "#ffa500", "#ffa500"];
 
 var socket = new WebSocket('ws://localhost:8000/gpu_status');
 socket.onopen = function(event){
@@ -12,19 +15,78 @@ socket.onclose = function(){
 }
 socket.onmessage = function(event){
     var gpu_status_data =JSON.parse(event.data);
-    // add the data to the graph, here.
-    for (hostname in gpu_status_data){
-        console.log("gpu data received:");
-        if (data[0].values.length == 240){
-            data[0].values.shift();
-        }
-        if (data[1].values.length == 240){
-            data[1].values.shift();
-        }
-        data[0].values.push({x: data[0].values.length, y: gpu_status_data[hostname]["1"]["u"]});
-        data[1].values.push({x: data[1].values.length, y: gpu_status_data[hostname]["2"]["u"]});
-        chart.update();
+    if (gpu_status_data["status"] == "initial"){
+        draw_initial(gpu_status_data["data"]);
+    }else if (gpu_status_data["status"] == "latest"){
+        console.log("received latest data");
     }
+}
+
+function draw_initial(data){
+    for (hostname in data){
+        chart_data[hostname] = {
+            "util": [],
+            "memory": []
+        };
+        chart[hostname] = {};
+
+        for (gpu_index in data[hostname]){
+            var values_u = [];
+            var values_m = [];
+            for (var i = 0; i < data[hostname][gpu_index]["time_stamp"].length; i++){
+                values_u.push({x: data[hostname][gpu_index]["time_stamp"][i] * 1000, y: data[hostname][gpu_index]["utilization_gpu"][i]});
+                values_m.push({x: data[hostname][gpu_index]["time_stamp"][i] * 1000, y: data[hostname][gpu_index]["utilization_memory"][i]});
+            }
+
+            chart_data[hostname]["util"].push(
+                {
+                    values: values_u,
+                    key: data[hostname][gpu_index]["gpu_name"] + "(" + gpu_index + ")",
+                    color: color_set[gpu_index]
+                }
+            );
+
+            chart_data[hostname]["memory"].push(
+                {
+                    values: values_u,
+                    key: data[hostname][gpu_index]["gpu_name"] + "(" + gpu_index + ")",
+                    color: color_set[gpu_index]
+                }
+            );
+        }
+
+        init_graph(hostname, "util");
+        init_graph(hostname, "memory");
+    }
+}
+
+function init_graph(hostname, monitor_type){
+    nv.addGraph(function() {
+        var c = nv.models.lineChart();
+        c.useInteractiveGuideline(true);
+        c.forceY([0, 100]);
+
+        c.xAxis.axisLabel("Time");
+        c.xAxis.tickFormat(function(d) {
+            return d3.time.format("%b %e %X")(new Date(d));
+        }).showMaxMin(false);
+        c.xAxis.rotateLabels(-10);
+
+        var y_label = monitor_type=="util"?"GPU Utilization (%)":"Memory Utilization (%)";
+        c.yAxis.axisLabel(y_label);
+        c.yAxis.tickFormat(d3.format('d'));
+
+        console.log("trying to add graph to: " + "#" + hostname + "." + monitor_type);
+
+        d3.select("#" + hostname).select("." + monitor_type).append('svg')
+        .datum(chart_data[hostname][monitor_type])
+        .call(c);
+
+        nv.utils.windowResize(c.update);
+
+        chart[hostname][monitor_type] = c;
+        return c;
+    });
 }
 
 var init = function(){
