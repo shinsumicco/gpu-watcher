b7f4b2fe10f37bdd99560ce1112266e9c81c9319
Kenta Urano <vrano@ucl.nuee.nagoya-u.ac.jp>
2018-04-27 15:45:05 +0900

    implemented graph update on websocket data receive

--- web/static/gpu_watcher.js
+++ web/static/gpu_watcher.js
@@ -1,6 +1,5 @@
 // Wrapping in nv.addGraph allows for '0 timeout render', stores rendered charts in nv.graphs, and may do more in the future... it's NOT required
 var chart = {};
-var data;
 var chart_data = {};
 
 // last 2 are dummy (for now)
@@ -18,7 +17,7 @@ socket.onmessage = function(event){
     if (gpu_status_data["status"] == "initial"){
         draw_initial(gpu_status_data["data"]);
     }else if (gpu_status_data["status"] == "latest"){
-        console.log("received latest data");
+        update_graph(gpu_status_data["data"]);
     }
 }
 
@@ -87,62 +86,27 @@ function init_graph(hostname, monitor_type){
     });
 }
 
-var init = function(){
-    nv.addGraph(function() {
-        chart = nv.models.lineChart()
-        .options({
-            duration: 300,
-            useInteractiveGuideline: true
-        })
-        ;
-        // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-        chart.xAxis
-        .axisLabel("Time (s)")
-        .tickFormat(d3.format(',.1f'))
-        .staggerLabels(true)
-        ;
-        chart.yAxis
-        .axisLabel('Voltage (v)')
-        .tickFormat(function(d) {
-            if (d == null) {
-                return 'N/A';
-            }
-            return d3.format(',.2f')(d);
-        })
-        ;
-        data = sinAndCos();
-        d3.select('#chart1').append('svg')
-        .datum(data)
-        .call(chart);
-        nv.utils.windowResize(chart.update);
-        return chart;
-    });
-}
+function update_graph(data){
+    for (hostname in data){
+        for (gpu_index in data[hostname]){
+            chart_data[hostname][gpu_index]["util"].values.shift();
+            chart_data[hostname][gpu_index["util"].values.push(
+                {
+                    x: data[hostname][gpu_index]["time_stamp"] * 1000,
+                    y: data[hostname][gpu_index]["utilization_gpu"]
+                }
+            );
 
-function sinAndCos() {
-    var sin = [],
-    sin2 = [],
-    cos = [],
-    rand = [],
-    rand2 = []
-    ;
-    for (var i = 0; i < 100; i++) {
-        sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) }); //the nulls are to show how defined works
-        cos.push({x: i, y: .5 * Math.cos(i/10)});
+            chart_data[hostname][gpu_index]["memory"].values.shift();
+            chart_data[hostname][gpu_index["memory"].values.push(
+                {
+                    x: data[hostname][gpu_index]["time_stamp"] * 1000,
+                    y: data[hostname][gpu_index]["utilization_memory"]
+                }
+            );
+
+            chart[hostname]["util"].update();
+            chart[hostname]["memory"].update();
+        }
     }
-    return [
-    {
-        area: true,
-        values: sin,
-        key: "Sine Wave",
-        color: "#ff7f0e",
-        strokeWidth: 4,
-        classed: 'dashed'
-    },
-    {
-        values: cos,
-        key: "Cosine Wave",
-        color: "#2ca02c"
-    }
-    ];
 }
